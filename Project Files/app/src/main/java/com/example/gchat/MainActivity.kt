4df688d7b7695f608f9a26708a571784e558983b
CharanR2021 <144363988+CharanR2021@users.noreply.github.com>
2023-11-07 12:48:48 +0530

    Add files via upload

---
+++ Project Files/app/src/main/java/com/example/gchat/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.google.firebase.FirebaseApp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        setContent {
+            NavComposeApp()
+        }
+    }
+}
---
+++ Project Files/app/src/main/java/com/example/gchat/nav/Navigation.kt
@@ -0,0 +1,33 @@
+package com.example.gchat.nav
+
+import androidx.navigation.NavHostController
+import com.example.gchat.nav.Destination.Home
+import com.example.gchat.nav.Destination.Login
+import com.example.gchat.nav.Destination.Register
+
+object Destination {
+    const val AuthenticationOption = "authenticationOption"
+    const val Register = "register"
+    const val Login = "login"
+    const val Home = "home"
+}
+
+/**
+ * Set of routes which will be passed to different composable so that
+ * the routes which are required can be taken.
+ */
+class Action(navController: NavHostController) {
+    val home: () -> Unit = {
+        navController.navigate(Home) {
+            popUpTo(Login) {
+                inclusive = true
+            }
+            popUpTo(Register) {
+                inclusive = true
+            }
+        }
+    }
+    val login: () -> Unit = { navController.navigate(Login) }
+    val register: () -> Unit = { navController.navigate(Register) }
+    val navigateBack: () -> Unit = { navController.popBackStack() }
+}
